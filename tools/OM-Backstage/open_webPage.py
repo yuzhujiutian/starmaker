43440f907badeb2de51e0b072093ad6566562ae1
yaoliang.cui <yaoliang.cui@ushow.media>
2019-04-24 15:37:33 +0800

    T31778 1.get_number

--- tools/OM-Backstage/open_webPage.py
+++ tools/OM-Backstage/open_webPage.py
@@ -5,7 +5,7 @@ import webbrowser
 a = 0
 num = 1
 while a < 100:
-    ExcelFile = xlrd.open_workbook(r'../OM-Backstage/15.xlsx')
+    ExcelFile = xlrd.open_workbook(r'../OM-Backstage/query_hive_47730.xlsx')
     sm_id = str(int(ExcelFile.sheet_by_index(0).row_values(num)[0]))
     path = "https://om.ushow.media/#/premium-detail?id=" + sm_id
     webbrowser.open(path)
