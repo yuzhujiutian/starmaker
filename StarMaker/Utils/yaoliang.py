b9cbf51b7efd3c14620ac1c641c3396709271e30
yaoliang.cui <yaoliang.cui@ushow.media>
2019-05-09 10:48:33 +0800

    T31778 PerformanceEvaluationTest

---
+++ StarMaker/Utils/yaoliang.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import time
+
+from StarMaker.Utils.GetAppiumDeriver import GetAppiumDeriver
+
+a = GetAppiumDeriver().driver
+time.sleep(20)
+
+
+if __name__ == '__main__':
+    pass
+
