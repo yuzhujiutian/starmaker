e0a9ed62f881e9aa8768e232f839b3edd5e8b933
yaoliang.cui <yaoliang.cui@ushow.media>
2019-06-24 21:41:39 +0800

    T31778 改为with open方法

--- StarMaker/Utils/Tools.py
+++ StarMaker/Utils/Tools.py
@@ -50,15 +50,12 @@ class Tools:
     def FindSource(Source_ID):
         global ReadMappingTable
         try:
-            ReadMappingTable = open("../bin/resource_mapping.txt_" + str(GetAppiumDeriver().Api), "r")
-            Result_ID = re.findall("R.id." + "(.*)""'", re.findall(
-                "R.id." + Source_ID + " -> " + "(.*)", ReadMappingTable.read()).__str__())[0]
-            return Result_ID
+            with open("../bin/resource_mapping.txt_" + str(GetAppiumDeriver().Api), "r") as ReadMappingTable:
+                Result_ID = re.findall("R.id." + "(.*)""'", re.findall(
+                    "R.id." + Source_ID + " -> " + "(.*)", ReadMappingTable.read()).__str__())[0]
+                return Result_ID
         except:
             return Source_ID
-        # finally:
-        #     if ReadMappingTable:
-        #         ReadMappingTable.close()
 
 
 # ----------
